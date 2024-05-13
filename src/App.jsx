a1eff797c5e177fff56d30a51f8e7f9db31d636b
FE_sunmin <fe.sunmin@gmail.com>
2024-05-13 20:54:09 +0900

    Feat: 과제 요구사항 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,75 @@ function App() {
   const [users, setUsers] = useState(initialState);
 
   // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
   const addUser = (e) => {
     e.preventDefault();
+
     // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
+    if (!name || !age) {
+      alert("이름과 나이 모두 입력해주세요");
+      return;
+    } 
 
     // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
+    const id = Date.now();
+
+    const userInfo = {
+      id: id,
+      name: name,
+      age: age
+    };
+
+    const userUpdate = [...users, userInfo];
+
+    setUsers(userUpdate);
+
+    setName("");
+    setAge("");
   };
 
   const removeUser = (id) => {
     // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    const userUpdate = users.filter(user => user.id !== id);
+    setUsers(userUpdate);
   };
 
   return (
     <>
       <h1>사용자 리스트</h1>
-      <form onSubmit={addUser}>
+      <form onSubmit={addUser} className="addForm">
         {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
-        <input type="text" placeholder="이름" />
-        <input type="number" placeholder="나이" />
-        <button type="submit">사용자 추가</button>
+        <div className="userInput">
+          <input 
+            type="text" 
+            placeholder="이름" 
+            className="nameInput"
+            value={name}
+            onChange={(e) => {setName(e.target.value)}}
+          />
+          <input 
+            type="number" 
+            placeholder="나이" 
+            className="ageInput"
+            value={age}
+            onChange={(e) => {setAge(e.target.value)}}
+          />
+        </div>
+        <button type="submit" className="addBtn">사용자 추가</button>
       </form>
-      <ul>
+      <ul className="userCard">
         {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
-        {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+        {users.map(user => (
+          <div className="userMap">
+            <li key={user.id} className="userData">
+              <pre>이름: {user.name}   |   나이: {user.age}</pre>
+            </li>
+            {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+            <button onClick={() => removeUser(user.id)} className="removeBtn">삭제</button>
+          </div>
+        ))}
       </ul>
     </>
   );
